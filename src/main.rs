c391d5c7de3b08834f7a3e4e4328c8ff70538f27
Mark Friedenbach <mark@friedenbach.org>
2025-04-02 22:29:57 -0700

    docs: improve crate and CLI documentation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
 // the MPL was not distributed with this file, You can obtain one at <http://mozilla.org/MPL/2.0/>.
 
+//! # Flow - A modern data pipeline workflow language
+//!
+//! Flow is a new scientific workflow language that combines the power of Rust's type system with
+//! container-based process isolation.  It provides a declarative way to define data processing
+//! pipelines while ensuring type safety and runtime isolation.
+
 use argh::FromArgs;
 
-/// Flow - A modern workflow engine
+/// Command line arguments for the flow engine
 #[derive(FromArgs)]
 struct Args {
     /// print version information
