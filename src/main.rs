be13e15f21b206d8230d222ef3f0eb85513a163b
Mark Friedenbach <mark@friedenbach.org>
2025-04-02 22:21:48 -0700

    feat: add basic CLI argument parsing with argh
    Add support for --version flag and automatic --help support using the argh
    crate for command-line argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
 // the MPL was not distributed with this file, You can obtain one at <http://mozilla.org/MPL/2.0/>.
 
+use argh::FromArgs;
+
+/// Flow - A modern workflow engine
+#[derive(FromArgs)]
+struct Args {
+    /// print version information
+    #[argh(switch)]
+    version: bool,
+}
+
 fn main() {
+    let args: Args = argh::from_env();
+
+    if args.version {
+        println!("flow {}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+
     println!("Hello, world!");
 }
 
